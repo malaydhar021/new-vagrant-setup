69fb66fcd68d2d30aec8f953860014f535db5a2e
Tier5 <work@tier5.us>
2019-02-20 23:01:10 +0530

    login page v0.8

--- app/scripts/src/app/components/login/login.component.ts
+++ app/scripts/src/app/components/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    document.body.className = "loginPage";
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy(){
+    document.body.className="loginPage";
   }
 
 }
