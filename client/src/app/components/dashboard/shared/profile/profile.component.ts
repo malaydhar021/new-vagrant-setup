fc9a4454f2375ff3877c930bb4386165469221a0
Tier5 LLC <work@tier5.us>
2019-04-16 18:37:32 +0530

    Edit Profile v0.3

--- client/src/app/components/dashboard/shared/profile/profile.component.ts
+++ client/src/app/components/dashboard/shared/profile/profile.component.ts
@@ -1,19 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators, FormsModule} from '@angular/forms';
 import { NgxSmartModalService } from 'ngx-smart-modal';
+import * as ValidationEngine from '../../../../helpers/form.helper';
+import { LoaderService } from '../../../../services/loader.service';
 @Component({
   selector: 'user-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  
+  userPasswordUpdateForm: FormGroup
+   /**
+   * Constructor to inject required service. It also subscribe to a observable which emits the current
+   * value of defined variable. 
+   * @constructor constructor
+   * @since Version 1.0.0
+   * @param ngxSmartModalService
+   * @param formBuilder 
+   * @param 
+   * @returns Void
+   */
   constructor(
     private ngxSmartModalService: NgxSmartModalService,
+    private formBuilder: FormBuilder,
+    private loaderService: LoaderService,
   ) { }
+  
+  createUserPasswordUpdateForm(isReset = false){
 
+    this.userPasswordUpdateForm = this.formBuilder.group({
+      current_password: ['', Validators.required],
+      new_password: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')
+      ])],
+      confirm_password: ['', Validators.required]
+    }, {
+      validator: ValidationEngine.MustMatch('new_password', 'confirm_password')
+    })
+
+    isReset && this.userPasswordUpdateForm.reset();
+  }
+
+
+  onUpdatePassword(){
+    let values = this.userPasswordUpdateForm.value;
+    this.loaderService.enableLoader();
+    setTimeout(()=>{
+      this.loaderService.disableLoader();
+      this.createUserPasswordUpdateForm(true);
+    },3000)
+    console.log(values);
+    
+  }
   ngOnInit() {
+    this.createUserPasswordUpdateForm();
   }
 
 }
