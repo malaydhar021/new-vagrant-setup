f09ea79128d5332b51c8ec922b00116a800103e6
jon vaughn <jon@thinkvaughn.com>
2019-05-28 23:04:32 +0530

    Minor changes with env for Widget and Exit popup

--- widget/src/main.js
+++ widget/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
-axios.defaults.baseURL = process.env.API_HOST
+axios.defaults.baseURL = process.env.VUE_APP_API_HOST
 axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 Vue.config.productionTip = false
