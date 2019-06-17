8291b430b1ed189db3f9eb41fe47f8948f1cd5cd
Jon Vaughn <work@tier5.us>
2019-06-17 13:50:09 +0530

    Final fixes v1.5

--- www/support.php
+++ www/support.php
@@ -7,8 +7,6 @@
     <div id="main-wrapper" class="spacingheader">
         <app-noticelabel [showNotice]="isMaintenance"></app-noticelabel>
         <style>
-
-
             /* iframe's parent node */
             div#root {
                 position: fixed;
@@ -17,7 +15,7 @@
             }
 
             /* iframe itself */
-            div#root > iframe {
+            div#root>iframe {
                 display: block;
                 width: 100%;
                 height: 80%;
@@ -33,9 +31,10 @@
         </style>
         <div id="root" class="supportContent">
             <iframe src="https://stickyreviews.nolt.io/" class="iframe-div"></iframe>
-            <div>
+            <div class="container">
                 <p>
-                    For any product related support please click on the messenger button showing on the right bottom corner of this page and connect with us.
+                    For any product related support please click on the messenger button showing on the right bottom
+                    corner of this page and connect with us.
                 </p>
             </div>
         </div>
