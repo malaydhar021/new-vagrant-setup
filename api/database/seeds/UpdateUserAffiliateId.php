22ee881a196a0f6e207ebd40217605728c9a55da
Tier5 LLC <github@tier5.us>
2019-08-27 16:13:10 +0530

    Modified the user affiliate seeder v1.0

--- api/database/seeds/UpdateUserAffiliateId.php
+++ api/database/seeds/UpdateUserAffiliateId.php
@@ -16,15 +16,33 @@ class UpdateUserAffiliateId extends Seeder
 
       $client = new \GuzzleHttp\Client();
       $url = getenv('AFFILIATE_URL');
+
+      $body = [
+        'productId'      => '6',
+      ];
+
+      $res = $client->post($url.'/hooks/fetchAccounts',[  'form_params'=> $body ]);
+      if($res->getStatusCode() == 200){
+        $response  = json_decode($res->getBody());
+        $usersAccounts =  $response->finalObject;
+      }
+
+      // get users and check with their email if found then update the affiliate_id
+      if(count($usersAccounts) != 0){
+        foreach ($usersAccounts as $usersAccount) {
+          User::where('email','LIKE','%'.$usersAccount->email.'%')->update(['affiliate_id' => $usersAccount->affiliateId]);
+        }
+      }
+
       $res = $client->get($url.'/hooks/affiliateId');
       if($res->getStatusCode() == 200){
         $response  = json_decode($res->getBody());
         $jonsAffiliateId =  $response->payload->rootUserAffiliateId;
       }
-
+      // update users who don't have affiliate id with default (jons affiliate id) affiliate id
       foreach (User::all() as $key => $user) {
         if ($user->affiliate_id == null ||  $user->affiliate_id == '') {
-          $user->update(['affiliate_id' => $jonsAffiliateId]);
+           $user->update(['affiliate_id' => $jonsAffiliateId]);
         }
       }
 
