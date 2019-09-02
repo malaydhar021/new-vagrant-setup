fd4a6732286f6090aa063a4967be32715593a876
Jon Vaughn <work@tier5.us>
2019-09-02 23:07:21 +0530

    Added og property to all public pages

--- www/header.php
+++ www/header.php
@@ -31,11 +31,17 @@ if($_GET['aid']){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta property="og:title" content="Sticky Reviews"/>
+    <meta property="og:type" content="categories"/>
+    <meta property="og:url" content="https://www.usestickyreviews.com/"/>
+    <meta property="og:image" content="https://www.usestickyreviews.com/images/Sticky-theme-5.png"/>
+    <meta property="og:site_name" content="Sticky Reviews"/>
+    <meta property="og:description" content="Sticky Reviews makes increasing conversions simple. It's super simple to set up and takes only around 10 minutes to get started."/>
     <!-- Favicons -->
     <link rel="shortcut icon" href="images/favicon.png">
     <link rel="shortcut icon" href="images/favicon.ico">
 
-    <title>Stickyreviews</title>
+    <title>Sticky Reviews</title>
 
     <link rel="stylesheet" href="style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
