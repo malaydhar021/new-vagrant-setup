b5101b63bb5c38b2831ba5d28a125fdfa4104857
Jon Vaughn <work@tier5.us>
2019-10-09 20:04:36 +0530

    Added beta environment to this app

---
+++ client/src/environments/environment.beta.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    maintenance: false,
+    debug : true,
+    APP_BASE_URL: 'https://app.beta.usestickyreviews.com',
+    API_BASE_URL: 'https://api.beta.usestickyreviews.com/v2',
+    WIDGET_BASE_URL: 'https://lib.beta.usestickyreviews.com/widget.min.js',
+    UPLOAD_BASE_URL: 'https://api.beta.usestickyreviews.com/uploads', // deprecated and will be removed in next release
+    CNAME_CONFIG_DOMAIN: 'cname.beta.usestickyreviews.com',
+    JONS_AFFILIATE_ID: '1451512084871700',
+    STARTER_PLAN_ID: 'plan_Fiwr24LcX6cp8S'
+};
